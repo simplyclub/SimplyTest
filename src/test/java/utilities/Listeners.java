4ac66d45c7a8b0062f2d32ce1e182424502f2361
SimplyMichaelYabkovitch <michael@simplyclub.co.il>
2021-12-08 10:50:25 +0200

    comment:  * update Email log text in listeners  * update Email log text in MainFunction.class
    Tests:
    
    class:
    
    update date :08/12/2021

--- src/test/java/utilities/Listeners.java
+++ src/test/java/utilities/Listeners.java
@@ -1,18 +1,11 @@
 package utilities;
 
 import Tests.BasePage;
-import com.aventstack.extentreports.*;
+import Utilities.LogFileHandling;
 //import com.aventstack.extentreports.service.ExtentTestManager;
-import com.aventstack.extentreports.ExtentTest;
-import com.aventstack.extentreports.Status;
-import okhttp3.OkHttpClient;
 import org.testng.ITestContext ;
 import org.testng.ITestListener ;
 import org.testng.ITestResult ;
-import org.testng.annotations.BeforeSuite;
-import org.testng.annotations.BeforeTest;
-
-import java.util.concurrent.TimeUnit;
 
 
 public class Listeners extends BasePage implements ITestListener  {
@@ -29,27 +22,91 @@ public class Listeners extends BasePage implements ITestListener  {
        //System.out.println("*********************~~~~~~**********************");
 
     }
+
+    @Override
+    public void onTestStart(ITestResult iTestResult) {
+
+    }
+
     @Override
     public void onTestSuccess(ITestResult arg0) {
         String str = arg0.getName();
 
         if (str.equals("subTotalTest")) {
             ExReApiTestReport.pass(arg0.getName() + " Test passed");
+            LogFileHandling.WriteToFile("EmailLog","V-"+arg0.getName() + " Test passed");
+            LogFileHandling.WriteToFile("EmailLog","    avgTimeSubTotal: "+ avgTimeSubTotal);
         }
         if (str.equals("tranEndTest")) {
             ExReAccumReport.pass(arg0.getName() + " Test passed");
+            LogFileHandling.WriteToFile("EmailLog","V-"+arg0.getName() + " Test passed");
+            LogFileHandling.WriteToFile("EmailLog","    avgTimeTranEnd: "+ avgTimeTrenEnd);
         }
         if (str.equals("tranRefundTest")) {
             ExReTernRefundReport.pass(arg0.getName() + " Test passed");
+            LogFileHandling.WriteToFile("EmailLog","V-"+arg0.getName() + " Test passed");
+            LogFileHandling.WriteToFile("EmailLog","    TimeTranRefund: "+ avgTimeTranRefund);
         }
-        if (str.equals("trenEndOnePhaseTest")) {
+        if (str.equals("tranEndOnePhaseTest")) {
             ExReTrenEndOnePhaseReport.pass(arg0.getName() + " Test passed");
+            LogFileHandling.WriteToFile("EmailLog","V-"+arg0.getName() + " Test passed");
+            LogFileHandling.WriteToFile("EmailLog","    TimeTrenEndOnePhase: "+ avgTimeTrenEndOnePhase);
         }
         if (str.equals("newMemberTests")) {
             ExReNewMemberTestReport.pass(arg0.getName() + " Test passed");
+            LogFileHandling.WriteToFile("EmailLog","V-"+arg0.getName() + " Test passed");
         }
-            if (str.equals("pointsValidityTest")) {
+        if (str.equals("pointsValidityTest")) {
             ExRePointsValiditReport.pass(arg0.getName() + "Test passed");
+            LogFileHandling.WriteToFile("EmailLog","V-"+arg0.getName() + " Test passed");
+        }
+        if (str.equals("MemberTest1")) {
+            ExReJoinSmsReport.pass(arg0.getName() + "Test passed");
+            LogFileHandling.WriteToFile("EmailLog","V-"+arg0.getName() + " Test passed");
+        }
+        if (str.equals("MemberTest2")) {
+            ExReJoinSmsReport.pass(arg0.getName() + "Test passed");
+            LogFileHandling.WriteToFile("EmailLog","V-"+arg0.getName() + " Test passed");
+        }
+        if (str.equals("MemberTest3")) {
+            ExReJoinSmsReport.pass(arg0.getName() + "Test passed");
+            LogFileHandling.WriteToFile("EmailLog","V-"+arg0.getName() + " Test passed");
+        }
+        if (str.equals("MemberTest4")) {
+            ExReJoinSmsReport.pass(arg0.getName() + "Test passed");
+            LogFileHandling.WriteToFile("EmailLog","V-"+arg0.getName() + " Test passed");
+        }
+        if (str.equals("MemberTest5")) {
+            ExReJoinSmsReport.pass(arg0.getName() + "Test passed");
+            LogFileHandling.WriteToFile("EmailLog","V-"+arg0.getName() + " Test passed");
+        }
+        if (str.equals("MemberTest6")) {
+            ExReJoinSmsReport.pass(arg0.getName() + "Test passed");
+            LogFileHandling.WriteToFile("EmailLog","V-"+arg0.getName() + " Test passed");
+        }
+        if (str.equals("MemberTest7")) {
+            ExReJoinSmsReport.pass(arg0.getName() + "Test passed");
+            LogFileHandling.WriteToFile("EmailLog","V-"+arg0.getName() + " Test passed");
+        }
+        if (str.equals("MemberTest8")) {
+            ExReJoinSmsReport.pass(arg0.getName() + "Test passed");
+            LogFileHandling.WriteToFile("EmailLog","V-"+arg0.getName() + " Test passed");
+        }
+        if (str.equals("JoinSms2Test")) {
+            ExReJoinSmsReport.pass(arg0.getName() + "Test passed");
+            LogFileHandling.WriteToFile("EmailLog","V-"+arg0.getName() + " Test passed");
+        }
+        if (str.equals("JoinSmsTest")) {
+            ExReJoinSmsReport.pass(arg0.getName() + "Test passed");
+            LogFileHandling.WriteToFile("EmailLog","V-"+arg0.getName() + " Test passed");
+        }
+        if (str.equals("MemberGetDetailsAndCodeTest")) {
+            ExReJoinSmsReport.pass(arg0.getName() + "Test passed");
+            LogFileHandling.WriteToFile("EmailLog","V-"+arg0.getName() + " Test passed");
+        }
+        if (str.equals("MemberGetDetailsTest")) {
+            ExReJoinSmsReport.pass(arg0.getName() + "Test passed");
+            LogFileHandling.WriteToFile("EmailLog","V-"+arg0.getName() + " Test passed");
         }
     }
 
@@ -62,27 +119,40 @@ public class Listeners extends BasePage implements ITestListener  {
     @Override
     public void onTestFailure(ITestResult arg0) {
 
-        //System.out.println("~~~~~~~~~~~~!!!!@@@@@ "+arg0+" ~~~~~~~~~~~~!!!!@@@@@ ");
-
         String str = arg0.getName();
 
         if (str.equals("subTotalTest")) {
             ExReApiTestReport.fail(arg0.getName() + " Test fail");
+            LogFileHandling.WriteToFile("EmailLog","X-"+arg0.getName() + " Test fail");
+
+
         }
         if (str.equals("tranEndTest")) {
             ExReAccumReport.fail(arg0.getName() + " Test fail");
+            LogFileHandling.WriteToFile("EmailLog","X-"+arg0.getName() + " Test fail");
+
         }
         if (str.equals("tranRefundTest")) {
             ExReTernRefundReport.fail(arg0.getName() + " Test fail");
+            LogFileHandling.WriteToFile("EmailLog","X-"+arg0.getName() + " Test fail");
+
         }
         if (str.equals("trenEndOnePhaseTest")) {
             ExReTrenEndOnePhaseReport.fail(arg0.getName() + " Test fail");
+            LogFileHandling.WriteToFile("EmailLog","X-"+arg0.getName() + " Test fail");
+
         }
         if (str.equals("newMemberTests")) {
             ExReNewMemberTestReport.fail(arg0.getName() + " Test fail");
+            LogFileHandling.WriteToFile("EmailLog","X-"+arg0.getName() + " Test fail");
         }
         if (str.equals("pointsValidityTest")) {
             ExRePointsValiditReport.fail(arg0.getName() + "Test fail");
+            LogFileHandling.WriteToFile("EmailLog","X-"+arg0.getName() + " Test fail");
+        }
+        if (str.equals("MemberGetDetailsAndCodeTest")) {
+            ExRePointsValiditReport.fail(arg0.getName() + "Test fail");
+            LogFileHandling.WriteToFile("EmailLog","X-"+arg0.getName() + " Test fail");
         }
 
 
